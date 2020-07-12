8ce68655d83e9ff52c577de9a4cf4a0cf047083e
blastsilver <blastsilver@gmail.com>
2020-07-12 20:23:09 +1000

    pushing changes to index.js

--- src/client/App/index.js
+++ src/client/App/index.js
@@ -1,47 +1,85 @@
-//~~ Dependencies ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
 import './styles.css'
 import Chat from '../Chat'
 import SignIn from "../SignIn"
 import SignUp from "../SignUp"
 // Requirements
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Firebase from '../../server/Firebase'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // Material UI - core
 import { CssBaseline, CircularProgress } from '@material-ui/core'
 
-//~~ Declarations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+export default function App() {
 
-export default class App extends React.Component {
+	const [firebaseInitialized, setFirebaseInitialized] = useState(false)
 
-	constructor(props) {
-		super(...arguments);
-		this.state = { ready: false };
+	useEffect(() => {
+		Firebase.IsInitialized().then(val => {
+			setFirebaseInitialized(val)
+		})
+	})
 
-		Firebase.IsInitialized().then(() => {
-			this.setState({ ready: true });
-		});
-	};
 
-	render() {
+	return firebaseInitialized !== false ? (
+		<div>
+			<CssBaseline />
+			<Router>
+				<Switch>
+					<Route path="/signin" component={SignIn} />
+					<Route path="/signup" component={SignUp} />
+					<Route path="/" component={Chat} />
+				</Switch>
+			</Router>
+		</div>
+	) : <div id="loader"><CircularProgress /></div>
+}
 
-		if (!this.state.ready) return (<div id="loader"><CircularProgress /></div>);
 
-		return (
-			<div>
-				<CssBaseline />
-				<Router>
-					<Switch>
-						<Route path="/signin" component={SignIn} />
-						<Route path="/signup" component={SignUp} />
-						<Route path="/" component={Chat} />
-					</Switch>
-				</Router>
-			</div>
-		);
-	}
 
-};
+// //~~ Dependencies ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// import './styles.css'
+// import Chat from '../Chat'
+// import SignIn from "../SignIn"
+// import SignUp from "../SignUp"
+// // Requirements
+// import React from 'react'
+// import Firebase from '../../server/Firebase'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// // Material UI - core
+// import { CssBaseline, CircularProgress } from '@material-ui/core'
+
+// //~~ Declarations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+// export default class App extends React.Component {
+
+// 	constructor(props) {
+// 		super(...arguments);
+// 		this.state = { ready: false };
+
+// 		Firebase.IsInitialized().then(() => {
+// 			this.setState({ ready: true });
+// 		});
+// 	};
+
+// 	render() {
+
+// 		if (!this.state.ready) return (<div id="loader"><CircularProgress /></div>);
+
+// 		return (
+// 			<div>
+// 				<CssBaseline />
+// 				<Router>
+// 					<Switch>
+// 						<Route path="/signin" component={SignIn} />
+// 						<Route path="/signup" component={SignUp} />
+// 						<Route path="/" component={Chat} />
+// 					</Switch>
+// 				</Router>
+// 			</div>
+// 		);
+// 	}
+
+// };
+
+// //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
