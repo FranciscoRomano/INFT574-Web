3a6f166253a39a73c8e4e32763f6ce2c612e9451
blastsilver <blastsilver@gmail.com>
2020-07-12 19:12:40 +1000

    Updates

---
+++ precache-manifest.f4b3f88de388f8e28c1ab9f2854f1905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c375a3e19c85471abc220844b9932d6f",
+    "url": "/INFT574-Web/index.html"
+  },
+  {
+    "revision": "9de7a8874a6ef8423469",
+    "url": "/INFT574-Web/static/css/main.3dd69007.chunk.css"
+  },
+  {
+    "revision": "c7e449326aba5b83daf8",
+    "url": "/INFT574-Web/static/js/2.32d2f59a.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/INFT574-Web/static/js/2.32d2f59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de7a8874a6ef8423469",
+    "url": "/INFT574-Web/static/js/main.e8107174.chunk.js"
+  },
+  {
+    "revision": "a5d3ac5539d6eddfaf63",
+    "url": "/INFT574-Web/static/js/runtime-main.1e888d3d.js"
+  }
+]);
