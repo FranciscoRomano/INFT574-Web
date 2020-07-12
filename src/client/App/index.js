00afb2cb13bd8dff7bd2070d8af8c054f29cd2ac
blastsilver <blastsilver@gmail.com>
2020-07-12 23:48:10 +1000

    few changes

--- src/client/App/index.js
+++ src/client/App/index.js
@@ -1,41 +1,3 @@
-// import './styles.css'
-// import Chat from '../Chat'
-// import SignIn from "../SignIn"
-// import SignUp from "../SignUp"
-// // Requirements
-// import React, { useState, useEffect } from 'react'
-// import Firebase from '../../server/Firebase'
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// // Material UI - core
-// import { CssBaseline, CircularProgress } from '@material-ui/core'
-
-// export default function App() {
-
-// 	const [firebaseInitialized, setFirebaseInitialized] = useState(false)
-
-// 	useEffect(() => {
-// 		Firebase.IsInitialized().then(val => {
-// 			setFirebaseInitialized(val)
-// 		})
-// 	})
-
-
-// 	return firebaseInitialized !== false ? (
-// 		<div>
-// 			<CssBaseline />
-// 			<Router>
-// 				<Switch>
-// 					<Route path="/signin" component={SignIn} />
-// 					<Route path="/signup" component={SignUp} />
-// 					<Route path="/" component={Chat} />
-// 				</Switch>
-// 			</Router>
-// 		</div>
-// 	) : <div id="loader"><CircularProgress /></div>
-// }
-
-
-
 //~~ Dependencies ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 import './styles.css'
@@ -53,7 +15,6 @@ import { CssBaseline } from '@material-ui/core'
 export default class App extends React.Component {
 
 	render() {
-
 		return (
 			<div>
 				<CssBaseline />
